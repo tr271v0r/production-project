dda69f67af03125ee39cf801bea4feca56029455
Trevor_Phillips <tr271v0r.phillips.industries@gmail.com>
2023-10-21 21:19:00 +0300

    Починил переводилку. Добавил обработку несуществующих маршрутов. Добавил CSS-loader

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import App from './app/App';
 
-// import "./shared/config/i18n/i18n"; //Влияет на размер бандла
+import './shared/config/i18n/i18n'; // Влияет на размер бандла
 
 render(
     <ThemeProvider>
