ace39e0194201d1371c315c0cbc794b13b2ae429
Trevor_Phillips <tr271v0r.phillips.industries@gmail.com>
2023-10-30 19:45:21 +0300

    fixed eslintrc & tsconfig; added the translations on links in navbar

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,10 +42,10 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', 
-            { 
-                markupOnly: true, ignoreAtribute: ['data-testid'] 
-            }
+        'i18next/no-literal-string': ['error',
+            {
+                markupOnly: true, ignoreAttribute: ['data-testid', 'to'],
+            },
         ],
         'linebreak-style': 'off', //! !!
         'max-len': ['error', { ignoreComments: true, code: 100 }],
@@ -58,7 +58,7 @@ module.exports = {
             files: ['**/src/**/*.test.{ts,tsx}'],
             rules: {
                 'i18next/no-literal-string': 'off',
-            }
-        }
-    ]
+            },
+        },
+    ],
 };
