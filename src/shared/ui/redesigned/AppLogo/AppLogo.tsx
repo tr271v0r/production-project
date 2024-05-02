c056e831e00cf512b2b347d8efae6e469a660672
Trevor_Phillips <tr271v0r.phillips.industries@gmail.com>
2024-05-02 17:20:40 +0300

    applogo was changed (updated z-index and type position); redesigned skeleton

--- src/shared/ui/redesigned/AppLogo/AppLogo.tsx
+++ src/shared/ui/redesigned/AppLogo/AppLogo.tsx
@@ -19,14 +19,14 @@ export const AppLogo = memo((props: AppLogoProps) => {
             justify="center"
             className={classNames(cls.appLogoWrapper, {}, [className])}
         >
-            <div className={cls.gradientBig} />
-            <div className={cls.gradientSmall} />
             <AppSvg
                 className={cls.appLogo}
                 width={size}
                 height={size}
                 color="black"
             />
+            <div className={cls.gradientBig} />
+            <div className={cls.gradientSmall} />
         </HStack>
     );
 });
